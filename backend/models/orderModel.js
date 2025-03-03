df13a429fd520f16b84fc5be3872959d8e19ac62
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-03 14:57:36 +0530

    add user seeder, update product data structure, and refactor model exports

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -76,4 +76,6 @@ const orderSchema = new mongoose.Schema({
     timestamps: true
 });
 
-export const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -75,4 +75,6 @@ const productSchema = new mongoose.Schema({
     timestamps: true
 });
 
-export const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
