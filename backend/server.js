81244c6a770469dc9c3a61f99cbd6e08ede1761e
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-11 17:52:12 +0530

    implement user authentication with login functionality and password matching

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ connectDB(); // connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('API is running!!!!!!');
 });
