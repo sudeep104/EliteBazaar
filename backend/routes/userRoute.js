a895637860b714d8ae4ca349f491bca26800ee60
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-12 10:59:13 +0530

    implement JWT authentication and protect routes with middleware

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,19 +1,29 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import {authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    getUserByID,
-    deleteUser,
-    updateUser} from '../controllers/userController.js'
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').get(getUserByID).delete(deleteUser).put(updateUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .get(protect, admin, getUserByID)
+  .delete(protect, admin, deleteUser)
+  .put(protect, admin, updateUser);
 
 export default router;
