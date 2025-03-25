44ceb36c84fb35b5a605e3ed42f5a933e60366d2
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-25 13:01:45 +0530

    implement order payment update functionality and adjust route methods

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoute from './routes/productRoute.js'
-import userRoute from './routes/userRoute.js';
-import orderRoute from './routes/orderRoute.js';
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoute from "./routes/productRoute.js";
+import userRoute from "./routes/userRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // connect to MongoDB
@@ -20,14 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running!!!!!!');
+app.get("/", (req, res) => {
+  res.send("API is running!!!!!!");
 });
 
-app.use('/api/products', productRoute);
-app.use('/api/users', userRoute);
-app.use('/api/orders', orderRoute);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
+app.use("/api/orders", orderRoute);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 app.use(notFound);
 app.use(errorHandler);
