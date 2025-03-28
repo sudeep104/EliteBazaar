0c36a46af827c20305b4c1cf1ec49846ff6dd388
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-28 17:15:34 +0530

    add functionality to update product details and create product edit screen

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      providesTags: ["Products"],
       keepUnusedDataFor: 5,
     }),
     getProductDetails: builder.query({
@@ -20,8 +21,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         url: PRODUCTS_URL,
         method: "POST",
       }),
-      invalidatesTags: ["Product"],
+      invalidatesTags: ["Products"],
     }),
+    updateProduct: builder.mutation({
+        query: (data) => ({
+          url: `${PRODUCTS_URL}/${data.productId}`,
+          method: "PUT",
+          body: data,
+        }),
+        invalidatesTags: ['Products'],
+    })
   }),
 });
 
@@ -29,4 +38,5 @@ export const {
   useGetProductsQuery,
   useGetProductDetailsQuery,
   useCreateProductMutation,
+    useUpdateProductMutation,
 } = productsApiSlice;
