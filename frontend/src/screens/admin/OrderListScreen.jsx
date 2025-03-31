6f152d1452435fe1800f1632285360051a9fdf9b
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-03-31 13:27:28 +0530

    add user management and update functionality with user list and edit screens

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -8,7 +8,6 @@ import { useGetOrdersQuery } from "../../slices/ordersApiSlice";
 
 const OrderListScreen = () => {
   const { data: orders, isLoading, error } = useGetOrdersQuery();
-  console.log(orders);
 
   return (
     <>
